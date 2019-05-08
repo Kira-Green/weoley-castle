d18a75ec84d69c7816ac7056e9599751944020e0
karenchahal <karenjeet.chahal@gmail.com>
2019-05-08 16:16:06 +0100

    added 6 images and portals to different scenes

---
+++ js/HelloSceneThree.js
@@ -0,0 +1,87 @@
+"use strict";
+
+import React, { Component } from "react";
+
+import { StyleSheet } from "react-native";
+
+import { ViroScene, Viro360Image, ViroText, ViroBox } from "react-viro";
+
+export default class HelloSceneThree extends Component {
+  constructor() {
+    super();
+
+    this.state = {}; // initialize state
+
+    this._showHelloWorldScene = this._showHelloWorldScene.bind(this);
+  }
+
+  backToBeach = () => {
+    this.props.sceneNavigator.push({ scene: require("./HelloWorldScene.js") });
+  };
+
+  sceneFour = () => {
+    this.props.sceneNavigator.push({ scene: require("./HelloSceneFour.js") });
+  };
+
+  render() {
+    return (
+      <ViroScene onClick={this._showHelloWorldScene}>
+        <Viro360Image source={require("./res/rhys.JPG")} />
+        <ViroText
+          text="Safe Giant Rhys"
+          width={1}
+          height={1}
+          position={[2, -0.6, -2]}
+          transformBehaviors={["billboard"]}
+          style={styles.helloWorldTextStyle}
+        />
+        <ViroText
+          text="Let's go and see Olivia"
+          width={1}
+          height={1}
+          position={[-2, 0.5, -2]}
+          transformBehaviors={["billboard"]}
+          style={styles.helloWorldTextStyle}
+        />
+
+        <ViroBox
+          position={[-3, 0, -2]}
+          scale={[0.5, 0.5, 0.2]}
+          materials={["grid"]}
+          onHover={this.sceneFour}
+        />
+        <ViroText
+          text="Go back to the beach"
+          width={1}
+          height={1}
+          position={[5, 0.5, -2]}
+          transformBehaviors={["billboard"]}
+          style={styles.helloWorldTextStyle}
+        />
+
+        <ViroBox
+          position={[5, 0, -2]}
+          scale={[0.5, 0.5, 0.2]}
+          materials={["grid"]}
+          onHover={this.backToBeach}
+        />
+      </ViroScene>
+    );
+  }
+
+  _showHelloWorldScene() {
+    this.props.sceneNavigator.pop();
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 15,
+    color: "#FFFF00",
+    textAlignVertical: "center",
+    textAlign: "center"
+  }
+});
+
+module.exports = HelloSceneThree;
--- js/HelloWorldScene.js
+++ js/HelloWorldScene.js
@@ -7,7 +7,9 @@ import {StyleSheet} from 'react-native';
 import {
   ViroScene,
   ViroText,
+  ViroBox,
   Viro360Image,
+  ViroMaterials
 } from 'react-viro';
 
 export default class HelloWorldScene extends Component {
@@ -16,13 +18,23 @@ export default class HelloWorldScene extends Component {
     super();
 
     this.state = {} // Set initial state here
+  
   }
 
+  sceneOne = () => {
+    this.props.sceneNavigator.push({ scene: require("./HelloSceneOne.js") });
+  }
   render() {
     return (
       <ViroScene>
         <Viro360Image source={require('./res/guadalupe_360.jpg')} />
-        <ViroText text="Hello Bootcampers!" width={2} height={2} position={[0, 0, -2]} style={styles.helloWorldTextStyle} />
+        <ViroText text="Follow the boxes to find new places!" width={2} height={2} position={[0, 0, -2]} style={styles.helloWorldTextStyle} />
+      <ViroBox
+          position={[0, -1, -2]}
+          scale={[0.5, 0.5, 0.2]}
+          materials={["grid"]}
+          onFuse={{callback:this.sceneOne, timeToFuse:1500}}
+        />
       </ViroScene>
     );
   }
@@ -32,11 +44,16 @@ export default class HelloWorldScene extends Component {
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
     fontFamily: 'Arial',
-    fontSize: 60,
+    fontSize: 30,
     color: '#ffffff',
     textAlignVertical: 'center',
     textAlign: 'center',  
   },
 });
 
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require("./res/castlelogo.png")
+  }
+});
 module.exports = HelloWorldScene;
